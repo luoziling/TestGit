1723a9b80aa35139633d182d04d716965fda3024
tom_glb <goodMorning_glb@atguigu.com>
2020-05-09 22:26:35 +0800

    test0.1

---
+++ src/main/java/com/wzb/git/Happy.java
@@ -0,0 +1,12 @@
+package com.wzb.git;
+
+/**
+ * @author Satsuki
+ * @time 2020/5/9 22:25
+ * @description:
+ */
+public class Happy {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
